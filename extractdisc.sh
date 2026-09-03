d801570b586a53b4e69dbea459cbe891c363efaf
beastofrock <45082804+beastofrock@users.noreply.github.com>
2026-09-03 13:57:57 +0200

    Create extractdisc.sh

---
+++ extractdisc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo"cat disc.cue"
+echo"FILE "disc.bin" BINARY"
+echo"  TRACK 01 MODE2/2352"
+echo"    INDEX 01 00:00:00"
+echo "actual file: "
+cat disc.cue
+#sudo apt update
+#sudo apt install bchunk
+bchunk disc.bin disc.cue disc
+7z e disc01.iso SYSTEM.CNF SYSTEM.CNF
+cat SYSTEM.CNF
+echo "###"
+bootfile=$(7z l disc01.iso | grep SLUS | cut -d' ' -f19)
+echo "$bootfile"
+7z e disc01.iso $bootfile $bootfile
+rm disc01.iso
